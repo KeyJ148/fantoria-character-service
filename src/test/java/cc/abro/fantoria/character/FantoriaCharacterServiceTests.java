db7e02f2bb9e91d32cab2f7618fb71c6e3de25b0
Evgeniy Serov <kejik148@gmail.com>
2025-05-13 01:05:02 +0500

    Fix gradle wrapper

--- src/test/java/cc/abro/fantoria/character/FantoriaCharacterServiceTests.java
+++ src/test/java/cc/abro/fantoria/character/FantoriaCharacterServiceTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.ActiveProfiles;
 class FantoriaCharacterServiceTests {
 
     /*@Test
-    void contextLoads() {
+    public void contextLoads() {
     }*/
 
 }
